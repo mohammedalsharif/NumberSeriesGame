46fec0782a2d2947537cbae2c79ea251a8dd7349
mohammed al sharif <ffmm972006@gmail.com>
2022-05-12 10:33:56 +0300

    initial Commit

---
+++ app/src/main/java/com/examples/numberseriesgame/ui/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.examples.numberseriesgame.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.examples.numberseriesgame.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
